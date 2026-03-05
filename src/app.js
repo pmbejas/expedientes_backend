385b9966495e7f18751cf3125972a668363915d4
pmbejas <pmbejas@gmail.com>
2026-03-05 19:10:23 -0300

    feat: commit de cambios generales

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",() => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
 
